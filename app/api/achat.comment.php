eeac5247b4a4ee58f8a1cfd1a16900d292bb54ba
Mamadou Coulibaly <seyuf@hotmail.com>
2013-03-08 12:32:52 +0100

    uploadFiles

--- app/api/achat.comment.php
+++ app/api/achat.comment.php
@@ -14,11 +14,11 @@ if(isset($_SESSION["Id"])){
 //get the value 
 $note     = isset($DATA -> { 'Note_Achat'  })? $DATA -> { 'Note_Achat'  } : null;
 $comment  = isset($DATA -> { 'Commentaire' })? $DATA -> { 'Commentaire' } : null;
-$idProjet = isset($DATA -> { 'ID_Projet'   })?  : null;
+$idProjet = isset($DATA -> { 'ID_Projet'   })? $DATA -> { 'ID_Projet'   } : null;
 
 
 //insert 
-$sql   =  " UPDATE INTO Achat SET Note_Achat = '$note', Commentaire = '$comment' WHERE ID_Projet = '$idProjet' " ;
+$sql   =  " UPDATE Achat SET Note_Achat = '$note', Commentaire = '$comment' WHERE ID_Projet = '$idProjet' " ;
 // check the insert
 if(mysqli_query( $db_conx,$sql )){
     echo json_encode(array('success' => ' Commentaire enregistré ')) ;
